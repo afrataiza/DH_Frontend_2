028d3f1565c29956fe1d756e401c671c145b8bca
Afra Taiza <afrataiza@gmail.com>
2023-03-01 21:40:40 -0300

    feat: implementa atividade da aula 6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import listPosts from './post.js';
+
 const themeButton = document.querySelector('#button-theme');
 console.log(themeButton);
 themeButton.addEventListener('click', (e) => {
@@ -9,4 +11,31 @@ themeButton.addEventListener('click', (e) => {
         e.target.textContent = 'toggle_on';
     }
     
+})
+
+const container = document.querySelector('.container')
+
+function createPost(image, title, description) {
+    const div = document.createElement('div');
+    div.className = 'item'
+    
+    const img = document.createElement('img');
+    img.src = image;
+    div.appendChild(img);
+
+    const h2 = document.createElement('h2');
+    h2.innerText = title;
+    div.appendChild(h2);
+
+    const p = document.createElement('p');
+    p.innerText = description;
+    div.appendChild(p);
+
+    return div;
+}
+
+
+listPosts.forEach(({ imgUrl, title, description }) => {
+   const post = createPost(imgUrl, title, description);
+   container.appendChild(post);
 })
